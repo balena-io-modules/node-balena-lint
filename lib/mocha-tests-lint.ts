e85abc4409c642cf73c6c3e2a0df48efea75f08d
Roman Mazur <mazur.roman@gmail.com>
2019-07-02 11:12:30 +0300

    Add mocha tests linting
    Change-type: minor
    Signed-off-by: Roman Mazur <roman@balena.io>

---
+++ lib/mocha-tests-lint.ts
@@ -0,0 +1,44 @@
+import * as Bluebird from 'bluebird';
+import * as fs from 'fs';
+
+// Mocha tests linter results.
+// When isError is set to true, the message contains details about found errors.
+export class MochaListResult {
+	constructor(
+		public readonly message: string,
+		public readonly isError: boolean,
+	) {}
+}
+
+const checkPattern = /(describe|it)\.only/;
+
+// Check if files in the test directory don't contain describe.only or it.only statements
+// to ensure that an accidental commit does not prevent all the tests from running.
+export async function lintMochaTests(
+	scripts: string[],
+): Promise<MochaListResult> {
+	let errorsFound = false;
+	let message = '';
+
+	const readFile = Bluebird.promisify(fs.readFile);
+
+	const allCheckPromises = scripts.map(async scriptPath => {
+		const content = (await readFile(scriptPath)).toString();
+		const lines = content.split('\n');
+		for (let ln = 0; ln < lines.length; ln++) {
+			const res = checkPattern.exec(lines[ln]);
+			if (res) {
+				errorsFound = true;
+				message += `File ${scriptPath}, line ${ln}: found ${res[0]}\n`;
+			}
+		}
+	});
+
+	try {
+		await Promise.all(allCheckPromises);
+		return new MochaListResult(errorsFound ? message : 'OK', errorsFound);
+	} catch (e) {
+		console.error(e);
+		return new MochaListResult(`Error reading input files: ${e.message}`, true);
+	}
+}
